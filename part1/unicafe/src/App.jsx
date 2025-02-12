6bba437d3981b26fec853af948eb748c217f6f41
Ovidiu Postoiu <postoiu.o@gmail.com>
2025-02-12 13:53:44 +0200

    Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,14 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({ good, neutral, bad }) => {
   const totalReviews = good + neutral + bad;
@@ -24,12 +31,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <Header title='statistics' />
-      <StatisticLine text='good' value= {good} />
-      <StatisticLine text='neutral' value= {neutral} />
-      <StatisticLine text='bad' value= {bad} />
-      <StatisticLine text='all' value= {totalReviews} />
-      <StatisticLine text='average' value= {average} />
-      <StatisticLine text='positive' value= {positiveFeedback} />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value= {good} />
+          <StatisticLine text='neutral' value= {neutral} />
+          <StatisticLine text='bad' value= {bad} />
+          <StatisticLine text='all' value= {totalReviews} />
+          <StatisticLine text='average' value= {average} />
+          <StatisticLine text='positive' value= {positiveFeedback} />
+        </tbody>
+      </table>
     </>
   )
 }
